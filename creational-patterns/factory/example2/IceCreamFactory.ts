61f892e366022f7d875ab12fe353a4c8173439be
herman <hermanconnor11@gmail.com>
2024-09-22 20:19:06 -0400

    Update example2 ice cream factory to show data-driven approach

--- creational-patterns/factory/example2/IceCreamFactory.ts
+++ creational-patterns/factory/example2/IceCreamFactory.ts
@@ -6,18 +6,41 @@ import Strawberry from './Strawberry';
 export default class IceCreamFactory {
   constructor() {}
 
+  // BASIC EXAMPLE
+  //
+  // public static createIcecream = (flavor: string): IceCream | null => {
+  //   switch (flavor.toLowerCase()) {
+  //     case 'chocolate':
+  //       return new Chocolate();
+  //     case 'vanilla':
+  //       return new Vanilla();
+  //     case 'strawberry':
+  //       return new Strawberry();
+  //     default:
+  //       return null;
+  //     // handle case where flavor not supported
+  //     // or maybe throw an error if flavor is not supported
+  //   }
+  // };
+
+  // If we wanted to extend our application with more flavors.
+  // Implementing a data-driven approach using a map or configuration object can make your factory method more scalable and maintainable as the flavor list grows.
+
+  // map of flavors to their respective constructors
+  //
+  private static flavors: Record<string, new () => IceCream> = {
+    chocolate: Chocolate,
+    vanilla: Vanilla,
+    strawberry: Strawberry,
+  };
+
   public static createIcecream = (flavor: string): IceCream | null => {
-    switch (flavor.toLowerCase()) {
-      case 'chocolate':
-        return new Chocolate();
-      case 'vanilla':
-        return new Vanilla();
-      case 'strawberry':
-        return new Strawberry();
-      default:
-        return null;
-      // handle case where flavor not supported
-      // or maybe throw an error if flavor is not supported
+    const FlavorClass = IceCreamFactory.flavors[flavor.toLowerCase()];
+
+    if (FlavorClass) {
+      return new FlavorClass();
+    } else {
+      return null;
     }
   };
 }
