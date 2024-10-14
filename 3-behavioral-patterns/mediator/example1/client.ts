0d38640bcd4d542f7b1570fa539480f957fb6e25
herman <hermanconnor11@gmail.com>
2024-10-13 20:02:07 -0400

    Add explanation to client file in example1 of mediator pattern

--- 3-behavioral-patterns/mediator/example1/client.ts
+++ 3-behavioral-patterns/mediator/example1/client.ts
@@ -3,6 +3,18 @@ import PlaneA from './PlaneA';
 import PlaneB from './PlaneB';
 import PlaneC from './PlaneC';
 
+/**
+In this example, we simulate a simple air traffic control system.The planes communicate only with the Air Traffic Control (ATC) system. The ATC coordinates messages between planes to avoid collisions, manage routes, etc.
+
+Components:
+  1. Air Traffic Controller (Mediator): Coordinates communication between planes.
+  2. Planes (Colleagues): Planes that need to receive and send messages related to air traffic and their own positions.
+
+Explanation:
+  Mediator (AirTrafficControl): The ATC system is the mediator that coordinates communication between the planes. Each plane only interacts with ATC, not with the other planes directly.
+  Planes (Colleagues): The planes send messages about their position, direction, altitude, etc., and ATC relays those messages to the other planes.
+ */
+
 const atc = new AirTrafficControl();
 
 // Create planes and register them with ATC
