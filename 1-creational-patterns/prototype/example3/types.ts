195eb261a01091b826efc74f97ae6f82023c343d
herman <hermanconnor11@gmail.com>
2024-10-26 17:20:52 -0400

    Remove example3 from prototype pattern

--- 1-creational-patterns/prototype/example3/types.ts
+++
@@ -1,7 +0,0 @@
-import MyDocument from './MyDocument';
-
-export type CloneMode = 1 | 2; // Mode 1 for shallow copy, Mode 2 for deep copy
-
-export interface IPrototype {
-  clone(mode: CloneMode): MyDocument;
-}
